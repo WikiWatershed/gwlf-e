07c8cef1ce9f3b86600208c98c759414e3f1af61
Kevin DeLoach <kdeloach@azavea.com>
2016-05-20 12:05:01 -0400

    Add `gmsval` method to enums
    We'll need this to serialize enum values to GMS format.
    
    Note that some enums also need an `intval` method because GMS uses
    different values to represent the same thing in several places.

--- gwlfe/enums.py
+++ gwlfe/enums.py
@@ -8,12 +8,28 @@ class YesOrNo(object):
     NO = '<No>'
     YES = '<Yes>'
 
-    @staticmethod
-    def parse(value):
+    @classmethod
+    def parse(cls, value):
         if value in ('0', 'N'):
-            return YesOrNo.NO
+            return cls.NO
         elif value in ('1', 'Y'):
-            return YesOrNo.YES
+            return cls.YES
+        raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def intval(cls, value):
+        if value == cls.NO:
+            return 0
+        elif value == cls.YES:
+            return 1
+        raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def gmsval(cls, value):
+        if value == cls.NO:
+            return 'N'
+        elif value == cls.YES:
+            return 'Y'
         raise ValueError('Unexpected value: ' + str(value))
 
 
@@ -21,13 +37,21 @@ class ETflag(object):
     HAMON_METHOD = '<Hamon method>'
     BLAINY_CRIDDLE_METHOD = '<Blainy-Criddle method>'
 
-    @staticmethod
-    def parse(value):
+    @classmethod
+    def parse(cls, value):
         value = int(value)
         if value == 0:
-            return ETflag.HAMON_METHOD
+            return cls.HAMON_METHOD
         elif value == 1:
-            return ETflag.BLAINY_CRIDDLE_METHOD
+            return cls.BLAINY_CRIDDLE_METHOD
+        raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def gmsval(cls, value):
+        if value == cls.HAMON_METHOD:
+            return 0
+        elif value == cls.BLAINY_CRIDDLE_METHOD:
+            return 1
         raise ValueError('Unexpected value: ' + str(value))
 
 
@@ -35,35 +59,47 @@ class GrowFlag(object):
     NON_GROWING_SEASON = '<Non-growing season>'
     GROWING_SEASON = '<Growing season>'
 
-    @staticmethod
-    def parse(value):
+    @classmethod
+    def parse(cls, value):
         value = int(value)
         if value == 0:
-            return GrowFlag.NON_GROWING_SEASON
+            return cls.NON_GROWING_SEASON
         elif value == 1:
-            return GrowFlag.GROWING_SEASON
+            return cls.GROWING_SEASON
         raise ValueError('Unexpected value: ' + str(value))
 
-    @staticmethod
-    def intval(value):
-        if value == GrowFlag.NON_GROWING_SEASON:
+    @classmethod
+    def intval(cls, value):
+        if value == cls.NON_GROWING_SEASON:
             return 0
-        elif value == GrowFlag.GROWING_SEASON:
+        elif value == cls.GROWING_SEASON:
             return 1
         raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def gmsval(cls, value):
+        return cls.intval(value)
 
 
 class SweepType(object):
     VACUUM = '<Vacuum>'
     MECHANICAL = '<Mechanical>'
 
-    @staticmethod
-    def parse(value):
+    @classmethod
+    def parse(cls, value):
         value = int(value)
         if value == 1:
-            return SweepType.MECHANICAL
+            return cls.MECHANICAL
         elif value == 2:
-            return GrowFlag.VACUUM
+            return cls.VACUUM
+        raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def gmsval(cls, value):
+        if value == cls.MECHANICAL:
+            return 1
+        elif value == cls.VACUUM:
+            return 2
         raise ValueError('Unexpected value: ' + str(value))
 
 
@@ -88,40 +124,78 @@ class LandUse(object):
     MD_RESIDENTIAL = '<Md_Residential>'
     HD_RESIDENTIAL = '<Hd_Residential>'
 
-    @staticmethod
-    def parse(value):
+    @classmethod
+    def parse(cls, value):
         if value == 'Water':
-            return LandUse.WATER
+            return cls.WATER
         if value in ('Hay/Past', 'Hay'):
-            return LandUse.HAY_PAST
+            return cls.HAY_PAST
         elif value == 'Cropland':
-            return LandUse.CROPLAND
+            return cls.CROPLAND
         elif value == 'Forest':
-            return LandUse.FOREST
+            return cls.FOREST
         elif value == 'Wetland':
-            return LandUse.WETLAND
+            return cls.WETLAND
         elif value in ('Disturbed', 'Disturbed Land'):
-            return LandUse.DISTURBED
+            return cls.DISTURBED
         elif value == 'Turfgrass':
-            return LandUse.TURFGRASS
+            return cls.TURFGRASS
         elif value in ('Open_Land', 'Open Land'):
-            return LandUse.OPEN_LAND
+            return cls.OPEN_LAND
         elif value in ('Bare_Rock', 'Bare Rock'):
-            return LandUse.BARE_ROCK
+            return cls.BARE_ROCK
         elif value in ('Sandy_Areas', 'Sandy Areas'):
-            return LandUse.SANDY_AREAS
+            return cls.SANDY_AREAS
         elif value in ('Unpaved_Road', 'Unpaved Roads'):
-            return LandUse.UNPAVED_ROAD
+            return cls.UNPAVED_ROAD
         elif value in ('Ld_Mixed', 'LD Mixed'):
-            return LandUse.LD_MIXED
+            return cls.LD_MIXED
         elif value in ('Md_Mixed', 'MD Mixed'):
-            return LandUse.MD_MIXED
+            return cls.MD_MIXED
         elif value in ('Hd_Mixed', 'HD Mixed'):
-            return LandUse.HD_MIXED
+            return cls.HD_MIXED
         elif value in ('Ld_Residential', 'LD Residential'):
-            return LandUse.LD_RESIDENTIAL
+            return cls.LD_RESIDENTIAL
         elif value in ('Md_Residential', 'MD Residential'):
-            return LandUse.MD_RESIDENTIAL
+            return cls.MD_RESIDENTIAL
         elif value in ('Hd_Residential', 'HD Residential'):
-            return LandUse.HD_RESIDENTIAL
+            return cls.HD_RESIDENTIAL
+        raise ValueError('Unexpected value: ' + str(value))
+
+    @classmethod
+    def gmsval(cls, value):
+        if value == cls.WATER:
+            return 'Water'
+        elif value == cls.HAY_PAST:
+            return 'Hay/Past'
+        elif value == cls.CROPLAND:
+            return 'Cropland'
+        elif value == cls.FOREST:
+            return 'Forest'
+        elif value == cls.WETLAND:
+            return 'Wetland'
+        elif value == cls.DISTURBED:
+            return 'Disturbed'
+        elif value == cls.TURFGRASS:
+            return 'Turfgrass'
+        elif value == cls.OPEN_LAND:
+            return 'Open_Land'
+        elif value == cls.BARE_ROCK:
+            return 'Bare_Rock'
+        elif value == cls.SANDY_AREAS:
+            return 'Sandy_Areas'
+        elif value == cls.UNPAVED_ROAD:
+            return 'Unpaved_Road'
+        elif value == cls.LD_MIXED:
+            return 'Ld_Mixed'
+        elif value == cls.MD_MIXED:
+            return 'Md_Mixed'
+        elif value == cls.HD_MIXED:
+            return 'Hd_Mixed'
+        elif value == cls.LD_RESIDENTIAL:
+            return 'Ld_Residential'
+        elif value == cls.MD_RESIDENTIAL:
+            return 'Md_Residential'
+        elif value == cls.HD_RESIDENTIAL:
+            return 'Hd_Residential'
         raise ValueError('Unexpected value: ' + str(value))
