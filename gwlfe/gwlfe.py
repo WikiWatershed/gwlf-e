4ef842ad9b763875e0fd79508d21fd1f5d339209
mikecampagna <msc94@drexel.edu>
2018-05-07 12:25:03 -0400

    Changing code back to original.

--- gwlfe/gwlfe.py
+++ gwlfe/gwlfe.py
@@ -506,7 +506,7 @@ def run(z):
                 if z.Temp[Y][i][j] > 0 and z.Water[Y][i][j] > 0.01:
                     CalcCnErosRunoffSed.CalcCN(z, i, Y, j)
                 else:
-                    print("mike's print statement")
+                    pass
                 # print("n-1 init snow (",Y,i,j,")",z.InitSnow)
 
                 # DAILY CN
