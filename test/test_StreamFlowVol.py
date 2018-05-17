17d4d1b1e3643d951015654742070d3d98bfb315
mudkipmaster <acg59@drexel.edu>
2018-05-17 15:39:16 -0400

    vectorized streamflow vol

--- test/test_StreamFlowVol.py
+++ test/test_StreamFlowVol.py
@@ -11,10 +11,18 @@ class TestStreamFlowVol(unittest.TestCase):
         input_file = open('input_4.gms', 'r')
         self.z = Parser.GmsReader(input_file).read()
 
-
-    @skip('Not Ready Yet.')
     def test_StreamFlowVol(self):
         z = self.z
         np.testing.assert_array_almost_equal(
-            StreamFlowVol.StreamFlowVol_2(),
-            StreamFlowVol.StreamFlowVol(), decimal=7)
+            StreamFlowVol.StreamFlowVol_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area,
+                                          z.CNI_0, z.AntMoist_0, z.Grow, z.CNP_0, z.Imper,
+                                          z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV, z.PcntET, z.DayHrs, z.MaxWaterCap,
+                                          z.SatStor_0, z.RecessionCoef, z.SeepCoef, z.Qretention, z.PctAreaInfil,
+                                          z.n25b, z.Landuse, z.TileDrainDensity, z.PointFlow, z.StreamWithdrawal,
+                                          z.GroundWithdrawal),
+            StreamFlowVol.StreamFlowVol(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area,
+                                        z.CNI_0, z.AntMoist_0, z.Grow, z.CNP_0, z.Imper,
+                                        z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV, z.PcntET, z.DayHrs, z.MaxWaterCap,
+                                        z.SatStor_0, z.RecessionCoef, z.SeepCoef, z.Qretention, z.PctAreaInfil,
+                                        z.n25b, z.Landuse, z.TileDrainDensity, z.PointFlow, z.StreamWithdrawal,
+                                        z.GroundWithdrawal), decimal=7)
