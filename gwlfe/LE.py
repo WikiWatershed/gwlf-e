db0be344111551b086a12d1694bbf8f71da772eb
mudkipmaster <acg59@drexel.edu>
2018-05-17 15:34:21 -0400

    vectorized LE

--- gwlfe/LE.py
+++ gwlfe/LE.py
@@ -25,5 +25,16 @@ def LE(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMois
     return result
 
 
-def LE_2():
-    pass
+def LE_2(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMoist_0, Grow, CNP_0, Imper,
+         ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap, SatStor_0, RecessionCoef, SeepCoef
+         , Qretention, PctAreaInfil, n25b, Landuse, TileDrainDensity, PointFlow, StreamWithdrawal, GroundWithdrawal
+         , NumAnimals, AvgAnimalWt, StreamFlowVolAdj, SedAFactor_0, AvKF, AvSlope, SedAAdjust):
+    sedafactor = SedAFactor(NumAnimals, AvgAnimalWt, NRur, NUrb, CNI_0, CNP_0, CN, Imper, Area, SedAFactor_0, AvKF,
+                            AvSlope, SedAAdjust)
+    streamflowvol = StreamFlowVol(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMoist_0, Grow,
+                                  CNP_0, Imper,
+                                  ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap, SatStor_0,
+                                  RecessionCoef, SeepCoef
+                                  , Qretention, PctAreaInfil, n25b, Landuse, TileDrainDensity, PointFlow,
+                                  StreamWithdrawal, GroundWithdrawal)
+    return sedafactor * StreamFlowVolAdj * streamflowvol ** 0.6
