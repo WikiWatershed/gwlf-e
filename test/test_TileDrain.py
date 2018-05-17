b67cb26083650801f4a27454b8a59f69c504002d
mudkipmaster <acg59@drexel.edu>
2018-05-17 15:58:41 -0400

    vectorized TileDrain

--- test/test_TileDrain.py
+++ test/test_TileDrain.py
@@ -11,10 +11,14 @@ class TestTileDrain(unittest.TestCase):
         input_file = open('input_4.gms', 'r')
         self.z = Parser.GmsReader(input_file).read()
 
-
-    @skip('Not Ready Yet.')
     def test_TileDrain(self):
         z = self.z
         np.testing.assert_array_almost_equal(
-            TileDrain.TileDrain_2(),
-            TileDrain.TileDrain(), decimal=7)
+            TileDrain.TileDrain_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0,
+                                  z.AntMoist_0, z.Grow, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV,
+                                  z.PcntET, z.DayHrs, z.MaxWaterCap, z.SatStor_0, z.RecessionCoef, z.SeepCoef,
+                                  z.Landuse, z.TileDrainDensity),
+            TileDrain.TileDrain(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0,
+                                z.AntMoist_0, z.Grow, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV,
+                                z.PcntET, z.DayHrs, z.MaxWaterCap, z.SatStor_0, z.RecessionCoef, z.SeepCoef,
+                                z.Landuse, z.TileDrainDensity), decimal=7)
