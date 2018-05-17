79f9020f5aeaee00d26d0a2b2de0b65e64a68e57
mudkipmaster <acg59@drexel.edu>
2018-05-17 16:57:59 -0400

    vectorized GrFlow

--- gwlfe/GrFlow.py
+++ gwlfe/GrFlow.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numba import jit
 from Timer import time_function
 from Percolation import Percolation
 from Memoization import memoize
@@ -11,8 +12,7 @@ def GrFlow(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, Ant
     deepseep = np.zeros((NYrs, 12, 31))
     satstor = np.zeros((NYrs, 12, 31))
     percolation = Percolation(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMoist_0, Grow, CNP_0,
-                              Imper,
-                              ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap)
+                              Imper, ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap)
     satstor_carryover = SatStor_0
     for Y in range(NYrs):
         for i in range(12):
@@ -27,5 +27,24 @@ def GrFlow(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, Ant
     return result
 
 
-def GrFlow_2():
-    pass
+@jit(cache=True, nopython=True)
+def GrFlow_inner(NYrs, SatStor_0, DaysMonth, RecessionCoef, SeepCoef, percolation):
+    result = np.zeros((NYrs, 12, 31))
+    satstor_carryover = SatStor_0
+    for Y in range(NYrs):
+        for i in range(12):
+            for j in range(DaysMonth[Y][i]):
+                result[Y][i][j] = RecessionCoef * satstor_carryover
+                satstor_carryover = satstor_carryover + percolation[Y][i][j] - result[Y][i][j] - SeepCoef * \
+                                    satstor_carryover
+                if satstor_carryover < 0:
+                    satstor_carryover = 0
+    return result
+
+
+def GrFlow_2(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMoist_0, Grow, CNP_0, Imper,
+             ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap, SatStor_0, RecessionCoef, SeepCoef):
+    percolation = Percolation(NYrs, DaysMonth, Temp, InitSnow_0, Prec, NRur, NUrb, Area, CNI_0, AntMoist_0, Grow, CNP_0,
+                              Imper, ISRR, ISRA, CN, UnsatStor_0, KV, PcntET, DayHrs, MaxWaterCap)
+
+    return GrFlow_inner(NYrs, SatStor_0, DaysMonth, RecessionCoef, SeepCoef, percolation)
--- test/test_GrFlow.py
+++ test/test_GrFlow.py
@@ -11,10 +11,12 @@ class TestGrFlow(unittest.TestCase):
         input_file = open('input_4.gms', 'r')
         self.z = Parser.GmsReader(input_file).read()
 
-
-    @skip('Not Ready Yet.')
     def test_GrFlow(self):
         z = self.z
         np.testing.assert_array_almost_equal(
-            GrFlow.GrFlow_2(),
-            GrFlow.GrFlow(), decimal=7)
+            GrFlow.GrFlow_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0,
+                            z.AntMoist_0, z.Grow, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV, z.PcntET,
+                            z.DayHrs, z.MaxWaterCap, z.SatStor_0, z.RecessionCoef, z.SeepCoef),
+            GrFlow.GrFlow(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0,
+                          z.AntMoist_0, z.Grow, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV, z.PcntET,
+                          z.DayHrs, z.MaxWaterCap, z.SatStor_0, z.RecessionCoef, z.SeepCoef), decimal=7)
