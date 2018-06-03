1354b07f083b26800f558dbd87bf269c0214d35c
BShakiba <bs643@drexel.edu>
2018-06-03 12:09:23 -0400

    AvErosion inputs changed

--- gwlfe/AnnualMeans.py
+++ gwlfe/AnnualMeans.py
@@ -141,8 +141,12 @@ def CalculateAnnualMeanLoads(z, Y):
                                    z.PctAreaInfil,
                                    z.n25b, z.CN, z.Landuse, z.TileDrainDensity))
     z.AvErosionSum = sum(
-        AvErosion_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.Acoef, z.NRur, z.KF, z.LS, z.C,
-                    z.P, z.Area))
+        AvErosion_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0, z.AntMoist_0,
+                                  z.Grow_0, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV, z.PcntET, z.DayHrs, z.MaxWaterCap,
+                                  z.SatStor_0, z.RecessionCoef, z.SeepCoef, z.Qretention, z.PctAreaInfil, z.n25b, z.Landuse, z.TileDrainDensity, z.PointFlow,
+                                  z.StreamWithdrawal, z.GroundWithdrawal, z.NumAnimals, z.AvgAnimalWt, z.StreamFlowVolAdj, z.SedAFactor_0,
+                                  z.AvKF, z.AvSlope, z.SedAAdjust, z.StreamLength, z.n42b, z.n46c, z.n85d, z.AgLength, z.n42, z.n45, z.n85, z.UrbBankStab,
+                                  z.SedDelivRatio_0, z.Acoef, z.KF, z.LS, z.C, z.P))
     z.AvSedYieldSum = sum(
         AvSedYield_2(z.NYrs, z.DaysMonth, z.Temp, z.InitSnow_0, z.Prec, z.NRur, z.NUrb, z.Area, z.CNI_0,
                      z.AntMoist_0, z.Grow_0, z.CNP_0, z.Imper, z.ISRR, z.ISRA, z.CN, z.UnsatStor_0, z.KV,
