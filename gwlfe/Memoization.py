2f4e75b38914b64da50d07a2a8722d01fde51253
mudkipmaster <austingentry@att.net>
2018-06-18 11:48:58 -0400

    cleaning up extra files

--- gwlfe/Memoization.py
+++ gwlfe/Memoization.py
@@ -2,13 +2,6 @@ import hashlib
 
 from numpy import ndarray
 
-
-# without
-# 300 loops of 'test_test', average time per loop: 0.321625, best: 0.303278, worst: 0.426772
-# 300 loops of 'test_test', average time per loop: 0.006865, best: 0.001217, worst: 0.016353
-# def memoize_with_args(f):
-#     return f
-
 def memoize_with_args(f):
     class memodict():
         def __init__(self, f):
@@ -30,32 +23,6 @@ def memoize_with_args(f):
                 return self.result[args_string]
 
     return memodict(f)
-
-
-# 300 loops of 'test_test', average time per loop: 0.000156, best: 0.000117, worst: 0.000521
-# def memoize(f):
-#     class memodict():
-#         def __init__(self, f):
-#             self.f = f
-#             self.result = None
-#         def __call__(self,*args):
-#             # if self.result is None:
-#             #     ret = self.result = self.f(*args)
-#             #     return ret
-#             return self.f(*args)
-#     return memodict(f)
-
-
-def memodict(f):
-    class memodict(dict):
-        def __missing__(self, *args):
-            ret = self['result'] = f(*args)
-            return ret
-
-        def __getitem__(self, *args):
-            return dict.__getitem__(self, 'result')
-
-    return memodict().__getitem__
 
 
 # def memoize(f): #use this function to disable memoization
