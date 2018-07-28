75bdae0b90e84d35c05087eb33c8bb76f51cd679
mudkipmaster <austingentry@att.net>
2018-07-28 19:21:07 -0400

    added multirun support to memoization

--- run.py
+++ run.py
@@ -20,23 +20,24 @@ def main():
     log.addHandler(ch)
 
     parser = argparse.ArgumentParser(description='Run the GWLF-E model for a specified gms file.')
-    parser.add_argument('input', type=str, help='Input GMS file')
-    parser.add_argument('output', type=str, nargs="?", help='Optional output file to write result of model run')
+    parser.add_argument('input', type=str, nargs="+", help='Input GMS file')
+    parser.add_argument('--output', type=str, nargs="?", help='Optional output file to write result of model run')
 
     args = parser.parse_args()
 
-    gms_filename = args.input
+    gms_filenames = args.input
 
-    fp = open(gms_filename, 'r')
-    z = Parser.GmsReader(fp).read()
-    start = time.time()
-    result, z = gwlfe.run(z)
-    if (args.output != None):  # gms out filename is sepcified so write
-        gmsout_filename = args.output
-        log.debug("Writing GMS output file (%s)" % (gmsout_filename))
-        with open(gmsout_filename, "w") as file:
-            test = Parser.GmsWriter(file)
-            test.write(z)
+    for gms_filename in gms_filenames:
+        fp = open(gms_filename, 'r')
+        z = Parser.GmsReader(fp).read()
+
+        result, z = gwlfe.run(z)
+        if (args.output != None):  # gms out filename is sepcified so write
+            gmsout_filename = args.output
+            log.debug("Writing GMS output file (%s)" % (gmsout_filename))
+            with open(gmsout_filename, "w") as file:
+                test = Parser.GmsWriter(file)
+                test.write(z)
 
 
 if __name__ == '__main__':
