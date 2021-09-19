abc031d3d38e5870a24d5b6406127920455e1dbf
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2021-09-19 12:52:35 -0400

    Add --json option to generate standard output

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ def main():
     parser = argparse.ArgumentParser(description='Run the GWLF-E model for a specified gms file.')
     parser.add_argument('input', type=str, nargs="+", help='Input GMS file')
     parser.add_argument('--output', type=str, nargs="?", help='Optional output file to write result of model run')
+    parser.add_argument('--json', type=str, nargs="?", help='Optional output file to write JSON result of model run')
 
     args = parser.parse_args()
 
@@ -38,6 +39,10 @@ def main():
             with open(gmsout_filename, "w") as file:
                 test = Parser.GmsWriter(file)
                 test.write(z)
+
+        if (args.json != None):
+            with open(args.json, "w") as file:
+                json.dump(result, file, indent=2, sort_keys=True)
 
 
 if __name__ == '__main__':
